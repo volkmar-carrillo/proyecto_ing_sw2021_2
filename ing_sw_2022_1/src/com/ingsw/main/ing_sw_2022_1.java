f109c29d3d1898074da894eaf6a02fd62bc81cea
volkmar-carrillo <carrillo.ramklov@gmail.com>
2022-06-07 18:03:53 -0500

    Creación de la Clase Main - Prueba

---
+++ ing_sw_2022_1/src/com/ingsw/main/ing_sw_2022_1.java
@@ -0,0 +1,11 @@
+package com.ingsw.main;
+
+/**
+ *
+ * @author Volkmar Carrillo
+ */
+public class ing_sw_2022_1 {
+    public static void main(String []args){
+        System.out.println("Hello World!");
+    }
+}
